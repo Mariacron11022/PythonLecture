4c2dd7c5520af07342805cc752095cda687d304b
Mariacron11022 <sutereo.137rr@icloud.com>
2023-03-05 17:32:23 +0900

    Lecture Day3

---
+++ basic/error.py
@@ -0,0 +1,7 @@
+# result = 100 / 0    # バグ　"bug"　<-debug
+# print("hello world!")
+
+result = "1" + "3"
+print(result)
+
+print(undefined_variable)
---
+++ basic/numeric.py
@@ -0,0 +1,28 @@
+# 数値型（Numeric）:integer, float, complex
+
+print(type(-100))
+
+print(type(1.3))
+print(0.1 + 0.1 + 0.1)
+
+print(1 + 0.4)
+print(1 - 0.4)
+print(2 * 3)
+print(1 / 2)
+print(5 * 6 - 3 / 6)
+result = 1 + 1.0
+print(f"type of result:{result} is {type(result)}")
+
+print(14 // 3)
+print(14 % 3)
+
+print(2 ** 3)
+
+hit_point = 100
+attack_point = 40.3
+remain = hit_point - attack_point
+print(f"remain hit point is {remain}")
+
+a = 1
+a += 1
+print(a)
